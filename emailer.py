a8ea02da398035bed72f720eeb72ca1fd664ed5b
ybadams <33207691+ybadams@users.noreply.github.com>
2019-03-04 17:08:24 -0500

    added comments

--- emailer.py
+++ emailer.py
@@ -1,10 +1,9 @@
-import smtplib
+import smtplib #used to send emails
 
-from string import Template
+from string import Template #used to create email template
 
-from email.mime.multipart import MIMEMultipart
+from email.mime.multipart import MIMEMultipart #email message object
 from email.mime.text import MIMEText
-from email.message import EmailMessage
 
 MY_ADDRESS = 'weatherandwords@gmail.com'
 PASSWORD = 'GiveusanA!'
@@ -24,16 +23,16 @@ def get_contacts(filename):
     return names, emails
 
 def read_template(filename):
-    """
-    Returns a Template object comprising the contents of the
-    file specified by filename.
-    """
+    """Returns a Template object comprising the contents of the
+    file specified by filename."""
 
     with open(filename, 'r', encoding='utf-8') as template_file:
         template_file_content = template_file.read()
     return Template(template_file_content)
 
 def main():
+    """Create new outgoing email server, create message containing weather 
+    and word of the day info, send email to all contacts from mycontacts.txt"""
     names, emails = get_contacts('mycontacts.txt') # read contacts
     message_template = read_template('message.txt')
 
@@ -42,7 +41,7 @@ def main():
     s.starttls()
     s.login(MY_ADDRESS, PASSWORD)
 
-    # For each contact, send the email:
+    # For each contact, send the personalized email:
     for name, email in zip(names, emails):
         msg = MIMEMultipart()       # create a message
 
@@ -52,19 +51,18 @@ def main():
         weather_contents = weather_file.read()
         
         # add in the actual person name to the message template, as well as wotd contents and weather data contents
-        message = message_template.substitute({'PERSON_NAME' : name.title(), 'W_OTD' : wotd_contents, 'WEATHER_UPDATE' : weather_contents})
-
-        print(message)
+        message = message_template.substitute({'PERSON_NAME' : name.title(), 
+                                               'W_OTD' : wotd_contents, 'WEATHER_UPDATE' : weather_contents})
 
         # setup the parameters of the message
         msg['From']=MY_ADDRESS
         msg['To']=email
-        msg['Subject']="Word of the Day!"
+        msg['Subject']="Daily Update!"
 
         # add in the message body
         msg.attach(MIMEText(message, 'plain'))
 
-        # send the message via the server set up earlie`1r.
+        # send the message, delete when sent.
         s.send_message(msg)
         del msg
 
