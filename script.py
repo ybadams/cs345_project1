dc9ef5ee060e404a134b9eeea07a922b078ba288
ybadams <33207691+ybadams@users.noreply.github.com>
2019-03-04 13:02:10 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,10 @@
+# Yuki Adams  Sara Salloum  Jimmy Nti-Kyeremeh
+# CS345 Project 1
+# Driver Script
+
+import webscraper
+import email_thing
+
+if __name__ == "__main__":
+    webscraper.main()
+    email_thing.main()
