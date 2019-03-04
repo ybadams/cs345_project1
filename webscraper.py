42bf39871862eefd5bbc0424b934cea001798ed9
ybadams <33207691+ybadams@users.noreply.github.com>
2019-03-04 12:58:18 -0500

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,71 +1,65 @@
-import requests #get(url)
-import re #regular expressions to find certain words/phrases
-import datetime #today's date/time
+import requests
+import re
+import datetime
 import sys
-import os #
+import os
 import subprocess
 import time
 import webbrowser
 
 def main():
-    """Scrape dictionary.com for word of the day info, overwrite scraped info to txt file, append to archive file."""
-    ## SETTING UP TXT FILE
-    locationOfYourScript = r'{}'.format(os.path.dirname(os.path.realpath(__file__)))
-
-    os.chdir(locationOfYourScript)
-    pathToTXTfile = r'{}\today.txt'.format(os.getcwd())
+    #path to text file is just file name (in working directory)
+    pathToTXTfile = 'today.txt'
 
     # Getting today's date
     today = datetime.date.today().strftime("%A %d %B %Y")
 
     # Overwriting existing files containing today's words
     with open(pathToTXTfile, 'w') as fileHandler:
-                    fileHandler.write("\t\t********************* {} *********************".format(today))
-                    fileHandler.write("\nWORD OF THE DAY\n")
+        fileHandler.write("\nWORDS OF THE DAY\n\n")
 
-
-
-    ## SCRAPING AND POPULATING TXT
     #4 THE FREE DICTIONARY
-print("Fetching word of the day from The Free Dictionary...")
+    print("Fetching word of the day from The Free Dictionary...")
 
-url = 'http://www.thefreedictionary.com/'
+    url = 'http://www.thefreedictionary.com/'
 
-source = requests.get(url)
+    source = requests.get(url)
 
-matchedObject = re.search(r'[\s\S\n]*?<a href=".*?/(.*?)">Definition</a>:</td>[\n\s]*?<td>(.*?)</td></tr>[\n\s]*?<tr>.*?Synonyms:.*?<td>(.*?)</td></tr>[\n\s]*?<tr>.*?Usage:.*?<td>(.*?)[\n\s]*?<a.*?>', source.text)
+    matchedObject = re.search(r'[\s\S\n]*?<a href=".*?/(.*?)">Definition</a>:</td>[\n\s]*?<td>(.*?)</td></tr>[\n\s]*?<tr>.*?Synonyms:.*?<td>(.*?)</td></tr>[\n\s]*?<tr>.*?Usage:.*?<td>(.*?)[\n\s]*?<a.*?>', source.text)
 
-try:
-    word = matchedObject.group(1)
-    meaning = matchedObject.group(2)
-    synonyms = matchedObject.group(3)
-    usage = matchedObject.group(4)
+    try:
+        word = matchedObject.group(1)
+        meaning = matchedObject.group(2)
+        synonyms = matchedObject.group(3)
+        usage = matchedObject.group(4)
 
-    cleanedWord = re.sub('<.*?>', '', word)
-    cleanedWord = re.sub('&#\d{2,4};', '', cleanedWord)
-    cleanedWord = re.sub('&nbsp;', ' ', cleanedWord)
+        cleanedWord = re.sub('<.*?>', '', word)
+        cleanedWord = re.sub('&#\d{2,4};', '', cleanedWord)
+        cleanedWord = re.sub('&nbsp;', ' ', cleanedWord)
 
-    cleanedMeaning = re.sub('<.*?>', '', meaning)
-    cleanedMeaning = re.sub('&#\d{2,4};', '', cleanedMeaning)
-    cleanedMeaning = re.sub('&nbsp;', ' ', cleanedMeaning)
+        cleanedMeaning = re.sub('<.*?>', '', meaning)
+        cleanedMeaning = re.sub('&#\d{2,4};', '', cleanedMeaning)
+        cleanedMeaning = re.sub('&nbsp;', ' ', cleanedMeaning)
 
-    cleanedSynonyms = re.sub('<.*?>', '', synonyms)
-    cleanedSynonyms = re.sub('&#\d{2,4};', '', cleanedSynonyms)
-    cleanedSynonyms = re.sub('&nbsp;', ' ', cleanedSynonyms)
+        cleanedSynonyms = re.sub('<.*?>', '', synonyms)
+        cleanedSynonyms = re.sub('&#\d{2,4};', '', cleanedSynonyms)
+        cleanedSynonyms = re.sub('&nbsp;', ' ', cleanedSynonyms)
 
-    cleanedUsage = re.sub('<.*?>', '', usage)
-    cleanedUsage = re.sub('&#\d{2,4};', '', cleanedUsage)
-    cleanedUsage = re.sub('&nbsp;', ' ', cleanedUsage)
+        cleanedUsage = re.sub('<.*?>', '', usage)
+        cleanedUsage = re.sub('&#\d{2,4};', '', cleanedUsage)
+        cleanedUsage = re.sub('&nbsp;', ' ', cleanedUsage)
 
-    entryForTextFile = "{}: {} Synonyms: {}. {}".format(cleanedWord, cleanedMeaning, cleanedSynonyms, cleanedUsage)
+        entryForTextFile = "{}: {} Synonyms: {}. {}".format(cleanedWord, cleanedMeaning, cleanedSynonyms, cleanedUsage)
 
-    with open(pathToTXTfile, 'a') as fileHandler:
-                fileHandler.write("- {}\n".format(entryForTextFile))
+        with open(pathToTXTfile, 'a') as fileHandler:
+                    fileHandler.write("- {}\n".format(entryForTextFile))
 
-except:
-    print("Error in retrieving information from The Free Dictionary [http://www.thefreedictionary.com/].", file = sys.stderr)
+    except:
+        print("Error in retrieving information from The Free Dictionary [http://www.thefreedictionary.com/].", file = sys.stderr)
 
-print("Done.")
-    
-if __name__=="__main__":
+
+    ## OPENING TEXT FILES, HTML AND THE CONTAINING FOLDER
+    print("Finished. \nCheck your email. :)")
+
+if __name__ == "__main__":
     main()
